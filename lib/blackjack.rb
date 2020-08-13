8df4f6b7f37e616f16507e3516330644e9660018
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-13 20:00:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,26 +8,30 @@ def deal_card
   puts rand(1..11)
 end
 
-def display_card_total(card_total)
+def display_card_total(card)
   # code #display_card_total here
+  card_total += card
   puts "Your card add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
-  
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  hit_or_stay = gets.chomp
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  
 end
 
 def hit?
